5189255c4e53d56107f3e682dbe95f92a9ee1ebc
mrattez <mrattez@student.42nice.fr>
2023-02-27 10:04:43 +0100

    ✅ Updated tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
-// #include "stack.hpp"
+#include "stack.hpp"
 #include "vector.hpp"
-#include "iterators.hpp"
-#include "is_integral.hpp"
-#include "enable_if.hpp"
+#include "utility.hpp"
 
 #include <iostream>
 #include <iomanip>
@@ -177,70 +175,13 @@ static void printDiff(std::vector<int> const &v1, std::vector<int> const &v2)
 	std::cout << s2 << std::endl;
 }
 
-std::vector<int>	g_range;
-
-template <class Vector>
-static std::vector<int> testVector(uint64_t &start, uint64_t &end, bool ft = false)
-{
-	std::vector<int>	results;
-	Vector				vector;
-	Vector				temp;
-
-	start = timer();
-
-	std::cout << "\n\e[47m " << (ft ? "ft::" : "std::") << "vector \e[0m\n"
-			  << std::endl;
-
-	temp.assign(26, 1);
-	vector.assign(42, 1);
-	vector.insert(vector.end() - 10, temp.begin(), temp.end());
-	results.push_back(vector[3]);
-	results.push_back(vector.size());
-	results.push_back(vector.capacity());
-
-	if (ft)
-	{
-		ft::vector<double>		ft_vector;
-		ft::vector<int>			ft_vector2;
-
-		ft_vector.assign(42, 1);
-		try
-		{
-			ft_vector2.insert(ft_vector2.begin(), ft_vector.begin(), ft_vector.end());
-		}
-		catch (...) {
-			results.push_back(ft_vector2.size());
-			results.push_back(ft_vector2.capacity());
-		}
-	}
-
-	end = timer();
-	std::cout << std::endl;
-	printExecutionTime(start, end);
-
-	return (results);
-}
-
 int main(void)
 {
 	(void)testIntegrals;
 
-	for (int i = 0; i < 100; ++i)
-		g_range.push_back(i);
+	ft::pair<std::string, int> p = ft::make_pair("Hello", 42);
+	std::cout << p.first << " " << p.second << std::endl;
 
-	uint64_t start;
-	uint64_t end;
-
-	std::vector<int> results[2];
-
-	results[0] = testVector<ft::vector<int> >(start, end, true);
-	results[1] = testVector<std::vector<int> >(start, end, false);
-
-	std::cout << "\n\e[47m Other tests \e[0m\n"
-			  << std::endl;
-
-	std::cout << "ft_vec == std_vec: " << emojiBoolean(results[0] == results[1]) << std::endl;
-	printDiff(results[0], results[1]);
 
 	return (EXIT_SUCCESS);
 }
